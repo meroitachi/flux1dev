94e94370cd9146b2fc26f205aa26e519a86af294
Mero Itachi <91469640+meroitachi@users.noreply.github.com>
2024-09-12 21:04:09 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const fetch = require("node-fetch");
+//const fetch = require("node-fetch");
 // Ensure to install this if not already
 const app = express();
 const port = 3000;
